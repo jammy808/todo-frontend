5339b73d4eda84fcdc98483ab7ca3caa79972bc4
Soham <jambhwadekarsoham@gmail.com>
2024-11-28 15:08:49 +0530

    api testing

--- src/app/utils/api.ts
+++ src/app/utils/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:5000',
+    baseURL: 'https://todo-backend-ashen-seven.vercel.app',
     withCredentials: true,
 });
 
